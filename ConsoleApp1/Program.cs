3ddb42030d5380476ed547abefaf67e6569fff7b
adelbajtikov <ba12139@auca.kg>
2024-06-08 12:37:36 +0600

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -9,4 +9,71 @@ namespace ConsoleApp1
             Console.WriteLine("Hello World!");
         }
     }
+     class LinkedList
+    {
+        private Node _head;
+        private Node _tail;
+        private int _count;
+ 
+        public LinkedList(Node node)
+        {
+            _head = node;
+            _tail = node;
+            _count++;
+        }
+ 
+        public void AddAfterLastNode(Node node)
+        {
+            _tail.next = node;
+            _tail = node;
+            _count++;
+        }
+ 
+        public void AddBeforeFirstNode(Node node)
+        {
+            node.next = _head;
+            _head = node;
+            _count++;
+        }
+ 
+        public void Add(Node givenNode, Node node)
+        {
+            //5 ->   3
+            node.next = givenNode.next;
+            //2 -> 5 
+            givenNode.next = node;
+            _count++;
+        }
+ 
+        public void DeleteLastNode()
+        {
+            Node current = _head;
+            while (current.next != _tail)
+            {
+                current = current.next;
+            }
+            _tail = current;
+            _tail.next = null;
+            _count--;
+        }
+ 
+ 
+        public void DeleteGivenNode(Node given)
+        {
+            Node current = _head;
+            while (current.next != given)
+            {
+                current = current.next;
+            }
+            current.next = given.next;
+            given.next = null;
+            _count--;
+        }
+ 
+        public void DeleteFirstNode()
+        {
+            Node temp = _head.next;
+            _head.next = null;
+            _head = temp;
+        }
 }
